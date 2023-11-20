43f5c7897381ea3c68da3c48eec48d6332b1fdfa
FrendaWinter <84429981+FrendaWinter@users.noreply.github.com>
2023-11-20 17:51:55 +0700

    Update solution for day 5 2015

--- 2015/ruby/sol_05.rb
+++ 2015/ruby/sol_05.rb
@@ -1,9 +1,18 @@
 file_path = '../data/test_data_05.txt'
 lines = File.readlines(file_path)
 
+def check_duplicates(input_string)
+    for i in 1..input_string.length do
+        if input_string[i] == input_string[i - 1]
+            return true
+        end
+    end
+    return false
+end
+
 result = 0
 lines.each do |line|
-    if line.scan(/[aeiou]/).count >= 3 && line.match(/\b\w*?(\w{2})\w*?\1\w*?\b/) && !line.match(/(ab|cd|pq|xy)/)
+    if line.scan(/[aeiou]/).count >= 3 && check_duplicates(line) && !line.match(/(ab|cd|pq|xy)/)
         result += 1
     end
 end
