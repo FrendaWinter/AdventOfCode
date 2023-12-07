a2dbba748d43325018f7021303f976a8b2096c63
Manh Duong <manh.duong@opswat.com>
2023-12-08 00:40:37 +0700

    Update Solution for day 6 2015

--- 2015/ruby/sol_06.rb
+++ 2015/ruby/sol_06.rb
@@ -1,11 +1,38 @@
 file_path = '../data/test_data_06.txt'
 lines = File.readlines(file_path)
 
+def turn_on(a, b, led_map)
+  p 'turn_on'
+end
+
+def turn_off(a, b, led_map)
+  p 'turn_off'
+end
+
+def toggle(a, b, led_map)
+  p 'toggle'
+end
+
+i = 0
 lines.each do |line|
     # Parse input
     one, two, three = line.match(/(turn\son|turn\soff|toggle)\s(\d+,\d+)\sthrough\s(\d+,\d+)/).captures
     puts '--------------------------------------'
-    p one
-    p two
-    p three
+    # p one
+    first_corner = two.split(',')
+    second_corner = three.split(',')
+    led_map = Array.new(1000) { Array.new(1000, '0')}
+
+    if one == 'turn on'
+      led_map = turn_on(first_corner, second_corner, led_map)
+    end
+    if one == 'turn off'
+      led_map = turn_off(first_corner, second_corner, led_map)
+    end
+    if one == 'toggle'
+      led_map = toggle(first_corner, second_corner, led_map)
+    end
+    
+    i += 1
+    break if i > 0
 end
