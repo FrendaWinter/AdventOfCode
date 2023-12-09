1d9abbc2f4b84e5db5b15160a13e917e47f89a54
FrendaWinter <84429981+FrendaWinter@users.noreply.github.com>
2023-12-09 11:13:20 +0700

    Update solution day 2 2023

--- 2023/sol_02.rb
+++ 2023/sol_02.rb
@@ -3,41 +3,81 @@ lines = File.readlines(file_path)
 
 ## Part 1
 sum = 0
-i = 0
+
 lines.each do |line|
-  # Parse input
-  break if i > 0
   id = line.split(':').first
   content = line.split(':').last
 
+  # Get ID number of Game
   id_number = id.match(/\d+/).to_s.to_i
-  p id_number
-  content = content.sub! "\n", ''
-  game_results = content.split(';')
-  p game_results
 
+  # Get all result of Game
+  content = content.sub! "\n", ''
+  # Split result
+  game_results = content.split(';')
+  is_posible = true
+  
   game_results.each do |game|
     ball_and_type = game.split(',')
-
+    # Init value
+    red_number = 0
+    green_number = 0
+    blue_number = 0    
+    # Get number of red, blue and green ball each game
     ball_and_type.each do |bat|
-      
       ball_number = bat.match(/\d+/).to_s.to_i
       ball_type = bat.match(/[a-z]+/).to_s
-
-      p ball_number
-      p ball_type
       
+
       red_number = ball_number if ball_type == 'red'
       green_number = ball_number if ball_type == 'green'
       blue_number = ball_number if ball_type == 'blue'
-
-      p red_number if red_number > 1 && !red_number.nil?
     end
-  end
 
-  i += 1
-  # two = line.scan(/\d/).last
-  # sum += (id).to_i
+    # Check if the game posible
+    is_posible = false if red_number > 12 || green_number > 13 || blue_number > 14
+  end
+  
+  sum += id_number.to_i if is_posible
+end
+
+p sum
+
+## Part 2
+sum = 0
+
+lines.each do |line|
+  content = line.split(':').last
+
+  # Get all result of Game
+  content = content.sub! "\n", ''
+  # Split result
+  game_results = content.split(';')
+
+  red_balls, green_balls, blue_balls = Array.new, Array.new, Array.new
+  
+  game_results.each do |game|
+    ball_and_type = game.split(',')
+    # Init value
+    red_number = 0
+    green_number = 0
+    blue_number = 0    
+    # Get number of red, blue and green ball each game
+    ball_and_type.each do |bat|
+      ball_number = bat.match(/\d+/).to_s.to_i
+      ball_type = bat.match(/[a-z]+/).to_s
+      
+
+      red_number = ball_number if ball_type == 'red'
+      green_number = ball_number if ball_type == 'green'
+      blue_number = ball_number if ball_type == 'blue'
+    end
+    red_balls.push(red_number)
+    green_balls.push(green_number)
+    blue_balls.push(blue_number)
+  end
+  
+  sum += red_balls.max * green_balls.max * blue_balls.max
 end
 
 p sum
