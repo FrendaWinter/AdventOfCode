83924ed6b94f2e67ef2485aa6fb6d0a9f0b4ce9e
FrendaWinter <84429981+FrendaWinter@users.noreply.github.com>
2024-03-19 19:25:57 +0700

    Add code

--- 2016/sol_1.cpp
+++ 2016/sol_1.cpp
@@ -6,17 +6,23 @@
 
 using namespace std;
 
-int main() {
+int main()
+{
   vector<string> lines = readFileLines("./data/input_1.txt");
 
-  if (lines.empty()) {
+  if (lines.empty())
+  {
     cerr << "File is emmpty" << endl;
     return 1;
   }
 
   vector<string> directions = split(lines[0], ",");
 
-  for(auto i : directions)
-    cout << i << endl;
+  for (auto i : directions)
+  {
+    string direction = i.substr(1, 1);
+
+    string step = i.substr(2, i.length() - 2);
+  }
   return 0;
 }
