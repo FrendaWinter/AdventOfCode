6884fdbe3546fbf7b43c5ed252189ba8091bbe11
Frenda Winter <gaconnoone1999@gmail.com>
2024-09-25 11:44:27 +0700

    update parsing input for d5 p1 2023

--- 2023/sol_05_part_1.cpp
+++ 2023/sol_05_part_1.cpp
@@ -8,6 +8,29 @@
 #include <sstream>
 using namespace std;
 
+void parse_input(const vector<string> &input, vector<tuple<string, string, string>> &output, string keyword) {
+	bool flag = false;
+	for (int i = 1; i < input.size(); i++) {
+		string line = input[i];
+
+		if (line == keyword + " map:") {
+			flag = true;
+			continue;
+		}
+
+		if (flag && !line.empty()) {
+			stringstream ss(line);
+			string des, source, result;
+			ss >> des >> source >> result;
+			output.push_back(make_tuple(des, source, result));
+		}
+
+		if (flag && line.empty()) {
+			flag = false;
+		}
+	}
+}
+
 int main(int argc, char *argv[]) {
 	// Part 2
 	vector<string> input;
@@ -38,38 +61,26 @@ int main(int argc, char *argv[]) {
 
 	// Out put seed list
 	for (int i = 0; i < input.size(); i++) {
-		cout << seed_list[i] << endl;
+		/*cout << seed_list[i] << endl;*/
 	}
 	
 	// Handle input seed to soil
 	vector<tuple<string, string, string>> seed_to_soil;
+	vector<tuple<string, string, string>> soil_to_fertilizer;
+	vector<tuple<string, string, string>> fertilizer_to_water;
+	vector<tuple<string, string, string>> water_to_light;
+	vector<tuple<string, string, string>> light_to_temperature;
+	vector<tuple<string, string, string>> temperature_to_humidity;
+	vector<tuple<string, string, string>> humidity_to_location;
 
-	bool is_sToS = false;
-	for (int i = 1; i < input.size(); i++) {
-		string line = input[i];
+	parse_input(input, seed_to_soil, "seed-to-soil");
+	parse_input(input, soil_to_fertilizer, "soil-to-fertilizer");
+	parse_input(input, fertilizer_to_water, "fertilizer-to-water");
+	parse_input(input, water_to_light, "water-to-light");
+	parse_input(input, light_to_temperature, "light-to-temperature");
+	parse_input(input, temperature_to_humidity, "temperature-to-humidity");
+	parse_input(input, humidity_to_location, "humidity-to-location");
 
-		if (line == "seed-to-soil map:") {
-			is_sToS = true;
-			continue;
-		}
-
-		if (is_sToS && line[0] >= '0' && line[0] <= '9') {
-			stringstream ss(line);
-			cout << line << endl;
-			string seed, soil, result;
-			ss >> seed >> soil >> result;
-			seed_to_soil.push_back(make_tuple(seed, soil, result));
-		}
-
-		if (is_sToS && line == "\n") {
-			is_sToS = false;
-		}
-	}
-
-	for (int i = 0; i < seed_to_soil.size(); i++) {
-		cout << get<0>(seed_to_soil[i]) << " " << get<1>(seed_to_soil[i]) << " " << get<2>(seed_to_soil[i]) << endl;
-	}
-
-	cout << endl;
+	cout << "Parsing input done" << endl;
 	return 0;
 }
