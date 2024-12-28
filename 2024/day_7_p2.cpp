c4e3c8191a7dc5821cde1f47bec88136f2f77dea
FrendaWinter <84429981+FrendaWinter@users.noreply.github.com>
2024-12-28 18:25:05 +0700

    Update day 7 p2 2024 done

--- 2024/day_7_p2.cpp
+++ 2024/day_7_p2.cpp
@@ -25,7 +25,29 @@ void parseInput(const std::string &input, std::pair<long int, std::vector<int>> 
     }
 };
 
-bool isMatch(const long int &base, const std::vector<int>& compare) {
+string convertToTernary(int n, const size_t &requiredLength)
+{
+    if (n == 0)
+        return std::string(requiredLength, '0');
+    
+    std::string str_ret;
+    while (n > 0)
+    {
+        int digitofternary = n % 3;
+        n /= 3;
+        std::string str = std::to_string(digitofternary);
+        str_ret.insert(0, str);
+    }
+
+    if (str_ret.length() == requiredLength)
+    {
+        return str_ret;
+    }
+
+    return std::string(requiredLength - str_ret.length(), '0') + str_ret;
+}
+
+bool isMatchBinary(const long int &base, const std::vector<int>& compare) {
 
     int exponent = compare.size() - 1;
 
@@ -57,48 +79,49 @@ bool isMatch(const long int &base, const std::vector<int>& compare) {
     return false;
 }
 
-std::vector<std::pair<long long, long long>> splitNumber(long long num)
-{
-    std::vector<std::pair<long long, long long>> result;
-    std::string numStr = std::to_string(num);
-    int len = numStr.length();
+bool isMatchTernary(const long int &base, const std::vector<int> &compare) {
+    int exponent = compare.size() - 1;
 
-    // Generate all possible splits
-    for (int i = 1; i < len; ++i)
+    for (int j = 0; j < pow(3, exponent); j++)
     {
-        long long firstPart = std::stoll(numStr.substr(0, i));
-        long long secondPart = std::stoll(numStr.substr(i));
+        std::string s = convertToTernary(j, exponent);
 
-        // Add the pair to the result
-        result.emplace_back(firstPart, secondPart);
+        long int tmp = compare[0];
+
+        if (base == 192) {
+            cout << s << endl;
+        }
+        for (int k = 0; k < compare.size() - 1; k++)
+        {
+            if (s[k] == '0')
+            {
+                tmp += compare[k + 1];
+            }
+
+            if (s[k] == '1')
+            {
+                tmp *= compare[k + 1];
+            }
+
+            if (s[k] == '2')
+            {
+                tmp = stol(std::to_string(tmp) + std::to_string(compare[k + 1]));
+            }
+        }
+
+        if (tmp == base)
+        {
+            return true;
+        }
     }
-
-    return result;
-}
-
-std::vector<int> getFirtHalfOfVector(const std::vector<int> &base, const int &i) {
-    std::vector<int> tmp;
-    for (int x = 0; x < i; x++){
-        tmp.push_back(base[i]);
-    }
-    return tmp;
-}
-
-std::vector<int> getSecondHalfOfVector(const std::vector<int> &base, const int &i)
-{
-    std::vector<int> tmp;
-    for (int x = i; x < base.size(); x++)
-    {
-        tmp.push_back(base[i]);
-    }
-    return tmp;
+    return false;
 }
 
 int main(int argc, char *argv[])
 {
     // Part 2
 	vector<string> input;
-	string filename = "data/input_test.txt";
+	string filename = "data/input_day_07.txt";
     ifstream input_file(filename);
 
     if (input_file.is_open())
@@ -125,25 +148,16 @@ int main(int argc, char *argv[])
     }
 
     for (int i = 0; i < mapSize; i++) {
-        if (isMatch(inputMap[i].first, inputMap[i].second)) {
+        if (isMatchBinary(inputMap[i].first, inputMap[i].second))
+        {
             result += inputMap[i].first;
-        } else {
-            auto pairs = splitNumber(inputMap[i].first);
-            for (const auto &pair : pairs)
-            {
-                for (int j = 1; j < inputMap[i].second.size(); j++) {
-                    std::vector<int> firstHalf = getFirtHalfOfVector(inputMap[i].second, j);
-                    std::vector<int> secondHalf = getSecondHalfOfVector(inputMap[i].second, j);
-
-                    bool isFirstPartMatch = isMatch(pair.first, firstHalf);
-                    bool isSecondPartMatch = isMatch(pair.second, secondHalf);
-
-                    if (isFirstPartMatch && isSecondPartMatch) {
-                        result += inputMap[i].first;
-                        break;
-                    }
-                }
-            }
+        }
+        else
+        {
+            if (isMatchTernary(inputMap[i].first, inputMap[i].second)) {
+                // cout << inputMap[i].first << endl;
+                result += inputMap[i].first;
+            };
         }
     }
     cout << "Result: " << result << endl;
