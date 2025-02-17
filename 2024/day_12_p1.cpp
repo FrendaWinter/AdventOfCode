1d497a51461fe4d1ebc626cbb55129c4bb3d0a11
Frenda Winter <gaconnoone1999@gmail.com>
2025-02-17 16:00:38 +0700

    Update sol for day 12 p1 2024

--- 2024/day_12_p1.cpp
+++ 2024/day_12_p1.cpp
@@ -15,27 +15,89 @@ const std::vector<std::pair<int, int>> directions = {
     {0, -1}, // Left
     {-1, 0}, // Up
 };
-vector<string> &garden;
-int result = 0;
 
-// Find area of char
+vector<string> garden;
+void markArea(const std::pair<int, int> &p);
+
+void harvestArea(const char &cmp, const std::pair<int, int> &p, vector<std::pair<int, int>> &area)
+{
+    markArea(p);
+
+    // Recursion to find all match
+    for (int i = 0; i < directions.size(); i++)
+    {
+        int x = p.first + directions[i].first;
+        int y = p.second + directions[i].second;
+
+        if (x < 0 || x >= garden.size() || y < 0 || y >= garden[0].size())
+        {
+            continue;
+        }
+
+        if (garden[x][y] == cmp)
+        {
+            area.push_back(std::make_pair(x, y));
+            harvestArea(cmp, std::make_pair(x, y), area);
+        }
+    }
+}
+
+// Find region that have the same char
 vector<std::pair<int, int>> findArea(const std::pair<int, int> &p)
 {
     char cmp = garden[p.first][p.second];
     vector<std::pair<int, int>> pArea;
+    pArea.push_back(p);
+    // Mark all land that have the same char with .
+    harvestArea(cmp, p, pArea);
+
+    return pArea;
 }
 
 // Mark area with dot
-void markArea(const vector<std::pair<int, int>> &area)
+void markArea(const std::pair<int, int> &p)
 {
-    for (size_t i = 0; i < area.size(); i++)
-    {
-        garden[area[i].first][area[i].second] = '.';
-    }
+    garden[p.first][p.second] = '.';
 };
 
 // Calculate perimeter
-int calPerimeters(vector<string> garden, vector<std::pair<int, int>> area);
+int calPerimeter(const vector<std::pair<int, int>> &area)
+{
+    int perimeter = 0;
+
+    for (int i = 0; i < area.size(); i++)
+    {
+        int fourAngle = 4;
+        for (int j = 0; j < directions.size(); j++)
+        {
+
+            int x = area[i].first + directions[j].first;
+            int y = area[i].second + directions[j].second;
+
+            if (x < 0 || x >= garden.size() || y < 0 || y >= garden[0].size())
+            {
+                continue;
+            }
+
+            if (std::find(area.begin(), area.end(), std::make_pair(x, y)) != area.end())
+            {
+                fourAngle--;
+            }
+        }
+        perimeter += fourAngle;
+    }
+    return perimeter;
+}
+
+// Cal price of the region.
+int calPrice(const vector<std::pair<int, int>> &area)
+{
+    int areaSize = area.size();
+
+    int perimeter = calPerimeter(area);
+
+    return areaSize * perimeter;
+}
 
 int main(int argc, char *argv[])
 {
@@ -48,7 +110,7 @@ int main(int argc, char *argv[])
         string line;
         while (std::getline(input_file, line))
         {
-            input.push_back(line);
+            garden.push_back(line);
         }
         input_file.close();
     }
@@ -57,14 +119,17 @@ int main(int argc, char *argv[])
         cerr << "Error when open file" << endl;
     }
 
-    vector<string> garden = input;
+    int result = 0;
     for (int i = 0; i < garden.size(); i++)
     {
         for (int j = 0; j < garden[i].length(); j++)
         {
             if (garden[i][j] != '.')
             {
-                findArea(std::make_pair(i, j));
+                // Find region
+                vector<std::pair<int, int>> pArea = findArea(std::make_pair(i, j));
+                // Cal price of that region and add to result
+                result += calPrice(pArea);
             }
         }
     }
